f411c2cf315ed89348f474a7d4a3fd3e9f1381b2
Vulug <>
2021-04-02 12:32:42 -0300

    Change logo on mobile

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -11,7 +11,7 @@ import TwitterCard from './components/TwitterCard'
 
 const Hero = styled.div`
   align-items: center;
-  background-image: url('/images/apwars/humans-group.png');
+  background-image: url('/images/apwars/humans-vs-orcs.png');
   background-repeat: no-repeat;
   background-position: top center;
   display: flex;
