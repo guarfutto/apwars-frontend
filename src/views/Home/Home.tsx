89243823a63635bc9e6c5b4db0311e402e3ac535
Vulug <>
2021-04-02 12:29:51 -0300

    Change home text

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -62,7 +62,7 @@ const Home: React.FC = () => {
         <Heading as="h1" size="xl" mb="24px" color="secondary">
           {TranslateString(576, 'APWars')}
         </Heading>
-        <Text>{TranslateString(578, 'Top 3 best DEFI app on Binance Smart Chain.')}</Text>
+        <Text>{TranslateString(578, 'Top best farming game on Binance Smart Chain.')}</Text>
       </Hero>
       <div>
         <Cards>
